1a5004023b5461a290fbd06d42a138dd966cd816
Philippe Conil <pconil@sdl.com>
2017-03-08 17:24:08 +0100

    initial commit
    Vimeo module for SDL Web DXA 1.7.
    Fixed width / height parameters for now.
    Dependant on Vimeo ECL Connector (the module uses the ECLItem DXA Model)

---
+++ Areas/Vimeo/VimeoAreaRegistration.cs
@@ -0,0 +1,23 @@
+using Sdl.Web.Common.Models;
+using Sdl.Web.Mvc.Configuration;
+
+namespace Sdl.Web.Modules.Vimeo
+{	
+    public class VimeoAreaRegistration : BaseAreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Vimeo";
+            }
+        }
+
+        protected override void RegisterAllViewModels()
+        {
+            // Entity Views
+            RegisterViewModel("VimeoPlayer", typeof(VimeoVideo));
+           
+        }
+    }
+}
---
+++ Model/VimeoVideo.cs
@@ -0,0 +1,10 @@
+using Sdl.Web.Common.Models;
+using Sdl.Web.Mvc.Configuration;
+
+namespace Sdl.Web.Modules.Vimeo
+{
+	[SemanticEntity(CoreVocabulary, "ExternalContentLibraryStubSchemavim")]
+	public class VimeoVideo : EclItem
+	{
+	}
+}
